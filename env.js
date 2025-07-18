246fd320ef542c0b1eeba0ff49148d5001d79bab
linklocalprj <linklocalprj@gmail.com>
2025-07-18 13:27:04 +0200

    Update env.js

--- env.js
+++ env.js
@@ -2,4 +2,3 @@
 export const SUPABASE_URL = 'https://jljljrkubullcrspicvj.supabase.co';
 export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impsamxqcmt1YnVsbGNyc3BpY3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1NTMzMzUsImV4cCI6MjA2NjEyOTMzNX0.mtQ9V2NgxPXTTg6x3uv9wL_Qz6WicnKxujiSIrpxpcw';
 export const SUPABASE_SERVICE_ROLE = process.env.SUPABASE_SERVICE_ROLE;
-export const UNSPLASH_API_KEY= '6VJ6gMwMqgaW8NujdoDKAjqfC0ZZU7sa2yAE9uhqw9o';

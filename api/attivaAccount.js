4e8422d6c51a01a21ec1da455b45fb8243eb6b56
linklocalprj <linklocalprj@gmail.com>
2025-07-24 13:55:09 +0200

    Update attivaAccount.js

--- api/attivaAccount.js
+++ api/attivaAccount.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE);
 
 export default async function handler(req, res) {
   const { email } = req.body;
